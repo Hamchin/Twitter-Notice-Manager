3652b496adfcc5ddd632d0041ed302780f16a499
KeyTey <k.terada.1211@gmail.com>
2020-02-26 00:24:16 +0900

    Update

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 import os, json, datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from requests_oauthlib import OAuth1Session
 
 app = Flask(__name__)
 CORS(app)
@@ -11,58 +12,80 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+CK = os.environ['TWITTER_CONSUMER_KEY']
+CS = os.environ['TWITTER_CONSUMER_SECRET']
+AT = os.environ['TWITTER_ACCESS_TOKEN']
+AS = os.environ['TWITTER_ACCESS_SECRET']
+twitter = OAuth1Session(CK, CS, AT, AS)
+
 # 通知テーブル
 class Notice(db.Model):
     __tablename__ = 'notices'
     id = db.Column(db.Integer, primary_key = True)
-    # 通知を受けたユーザーネーム
-    receive_user = db.Column(db.String(), nullable = False)
-    # 通知を送ったユーザーネーム
-    send_user = db.Column(db.String(), nullable = False)
+    # 通知を受けたユーザー
+    receiver_id = db.Column(db.String(), nullable = False)
+    # 通知を送ったユーザー
+    sender_id = db.Column(db.String(), nullable = False)
     # 通知対象のツイートID
     tweet_id = db.Column(db.String(), nullable = False)
     # 通知時間
-    datetime = db.Column(db.String(), nullable = False)
+    timestamp = db.Column(db.Integer, nullable = False)
     # データを辞書型として取得
     def get_dict(self):
         data = {
             'id': self.id,
-            'receive_user': self.receive_user,
-            'send_user': self.send_user,
+            'receiver_id': self.receiver_id,
+            'sender_id': self.sender_id,
             'tweet_id': self.tweet_id,
-            'datetime': self.datetime
+            'timestamp': self.timestamp
         }
         return data
 
-# 日付整形
-def format_date(date):
+# タイムスタンプ取得
+def get_timestamp(date):
     date = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.000Z")
     date = date + datetime.timedelta(hours = 9)
-    date = date.strftime("%Y-%m-%d %H:%M:%S")
-    return date
+    timestamp = int(date.timestamp())
+    return timestamp
 
 # 重複チェック
 def exist(notice):
     count = db.session.query(Notice).filter(
-        Notice.receive_user == notice.receive_user,
-        Notice.send_user == notice.send_user,
+        Notice.receiver_id == notice.receiver_id,
+        Notice.sender_id == notice.sender_id,
         Notice.tweet_id == notice.tweet_id
     ).count()
     return count > 0
 
+# ユーザーID取得
+def get_user(name):
+    url = "https://api.twitter.com/1.1/users/show.json"
+    params = {
+        'screen_name': name,
+        'include_entities': False
+    }
+    res = twitter.get(url, params = params, timeout = 10)
+    if res.status_code != 200: return {}
+    return json.loads(res.text)
+
 # 通知追加
 def add_notice(data):
-    notice = Notice()
-    receive_user = data.get('receive_user')
-    send_user = data.get('send_user')
+    # パラメータ読み込み
+    receiver = data.get('receiver')
+    sender = data.get('sender')
     tweet_id = data.get('tweet_id')
     date = data.get('datetime')
-    if not (receive_user and send_user and tweet_id and date): return {}
-    date = format_date(date)
-    notice.receive_user = receive_user
-    notice.send_user = send_user
+    if not (receiver and sender and tweet_id and date): return {}
+    # ユーザーネームからユーザーID取得
+    receiver_id = get_user(receiver).get('id_str')
+    sender_id = get_user(sender).get('id_str')
+    if not (receiver_id and sender_id): return {}
+    # 通知作成 -> 通知追加
+    notice = Notice()
+    notice.receiver_id = receiver_id
+    notice.sender_id = sender_id
     notice.tweet_id = tweet_id
-    notice.datetime = date
+    notice.timestamp = get_timestamp(date)
     if exist(notice): return {}
     db.session.add(notice)
     db.session.commit()
@@ -70,38 +93,20 @@ def add_notice(data):
 
 # 通知取得
 def get_notices(size):
-    notices = db.session.query(Notice).order_by(Notice.id.desc()).limit(size).all()
+    notices = db.session.query(Notice).order_by(Notice.timestamp.desc()).limit(size).all()
     notices = [notice.get_dict() for notice in notices]
-    notices = sorted(notices, key = lambda notice: notice['id'])
     return notices
-
-# 指定日数以前の通知ID取得
-def get_timeover_ids(day):
-    ids = []
-    notices = db.session.query(Notice).all()
-    standard = datetime.datetime.now()
-    for notice in notices:
-        date = datetime.datetime.strptime(notice.datetime, "%Y-%m-%dT%H:%M:%S.000Z")
-        date = date + datetime.timedelta(hours = 9)
-        if standard - datetime.timedelta(days = day) > date: ids.append(notice.id)
-    return ids
 
 # トップページ
 @app.route('/')
 def index():
-    notices = get_notices(100)
-    return render_template('index.html', notices = reversed(notices))
-
-# 通知追加ルーティング
-@app.route('/notice', methods = ['POST'])
-def post_notice():
-    notice = add_notice(request.form)
-    return redirect(url_for('index'))
+    notices = get_notices(size = 100)
+    return render_template('index.html', notices = notices)
 
 # 通知取得API
 # size: 通知取得数
-@app.route('/api/notices', methods = ['GET'])
-def api_get_notices():
+@app.route('/notices', methods = ['GET'])
+def get_notices():
     size = request.args.get('size', 10)
     notices = get_notices(size)
     return json.dumps(notices, indent = 4)
@@ -111,8 +116,8 @@ def api_get_notices():
 # send_user: 通知の送信ユーザー
 # tweet_id: ツイートID
 # datetime: タイムスタンプ
-@app.route('/api/notice/create', methods = ['GET'])
-def api_create_notice():
+@app.route('/notice/create', methods = ['GET'])
+def create_notice():
     notice = add_notice(request.args)
     return json.dumps(notice, indent = 4)
 
@@ -121,14 +126,14 @@ def api_create_notice():
 # send_user: 通知の送信ユーザー
 # tweet_id: ツイートID
 # datetime: タイムスタンプ
-@app.route('/api/notice', methods = ['POST'])
-def api_post_notice():
+@app.route('/notice', methods = ['POST'])
+def post_notice():
     notice = add_notice(request.get_json())
     return json.dumps(notice, indent = 4)
 
 # 通知削除API
 # id: 対象ID
-@app.route('/api/notice', methods = ['DELETE'])
+@app.route('/notice', methods = ['DELETE'])
 def delete_notice():
     req = request.get_json()
     data = db.session.query(Notice).filter(Notice.id == req['id']).first()
