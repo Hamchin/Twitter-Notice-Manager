c7df8dab54caae5dd67b25c7fb6c5aec729cc848
KeyTey <k.terada.1211@gmail.com>
2020-09-16 00:00:00 +0900

    Update

--- backend/twitter.py
+++ backend/twitter.py
@@ -1,52 +1,63 @@
-import os, json
+import os
 from requests_oauthlib import OAuth1Session
 
 CK = os.environ['TWITTER_CONSUMER_KEY']
 CS = os.environ['TWITTER_CONSUMER_SECRET']
 AT = os.environ['TWITTER_ACCESS_TOKEN']
 AS = os.environ['TWITTER_ACCESS_SECRET']
-twitter = OAuth1Session(CK, CS, AT, AS)
+session = OAuth1Session(CK, CS, AT, AS)
 
 # ユーザーを取得する
-def get_user(user_id = '', screen_name = ''):
+def get_user(user_id = None, screen_name = None):
     url = 'https://api.twitter.com/1.1/users/show.json'
-    key = 'user_id' if user_id else 'screen_name'
-    value = user_id if user_id else screen_name
-    params = {key: value, 'include_entities': False}
+    params = {
+        'user_id': user_id,
+        'screen_name': screen_name,
+        'skip_status': True,
+        'include_user_entities': False
+    }
     try:
-        res = twitter.get(url, params = params, timeout = 10)
+        res = session.get(url, params = params)
     except Exception as error:
         print(error)
         return None
     if res.status_code != 200: return None
-    return json.loads(res.text)
+    return res.json()
 
 # 複数のユーザーを取得する
 def get_users(user_ids = [], screen_names = []):
     url = 'https://api.twitter.com/1.1/users/lookup.json'
-    key = 'user_id' if user_ids != [] else 'screen_name'
-    values = user_ids if user_ids != [] else screen_names
-    params = {key: ','.join(values), 'include_entities': False}
+    if user_ids != []:
+        user_key = 'user_id'
+        user_values = user_ids
+    else:
+        user_key = 'screen_name'
+        user_values = screen_names
+    params = {
+        user_key: ','.join(user_values),
+        'skip_status': True,
+        'include_user_entities': False
+    }
     try:
-        res = twitter.get(url, params = params, timeout = 10)
+        res = session.get(url, params = params)
     except Exception as error:
         print(error)
         return []
     if res.status_code != 200: return []
-    return json.loads(res.text)
+    return res.json()
 
 # 複数のツイートを取得する
-def get_tweets(tweet_ids):
+def get_tweets(tweet_ids = []):
     url = 'https://api.twitter.com/1.1/statuses/lookup.json'
     params = {
         'id': ','.join(tweet_ids),
-        'include_entities': False,
-        'trim_user': True
+        'trim_user': True,
+        'include_entities': False
     }
     try:
-        res = twitter.get(url, params = params, timeout = 10)
+        res = session.get(url, params = params)
     except Exception as error:
         print(error)
         return []
     if res.status_code != 200: return []
-    return json.loads(res.text)
+    return res.json()
